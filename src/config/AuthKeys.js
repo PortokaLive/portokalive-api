f1ef1d7475a4b18cb0ad5ecef40ec9e8376e0d26
m3yevn <mmm.myintmyat@gmail.com>
2020-05-10 00:03:08 +0800

    ♨️ remove: unrequired js files

--- src/config/AuthKeys.js
+++
@@ -1,8 +0,0 @@
-var dotenv_1 = require("dotenv");
-var path_1 = require("path");
-dotenv_1.config({ path: path_1.resolve(__dirname, "../.env") });
-exports["default"] = {
-    mongoURI: process.env.MONGO_URI || "",
-    port: process.env.PORT || 5000,
-    secretKey: process.env.SECRET_KEY || ""
-};
--- src/utils/handleNotFound.js
+++
@@ -1,3 +0,0 @@
-exports.handleNotFound = function (_, res) {
-    res.status(404).json({ error: 404, message: "Route not found" });
-};
