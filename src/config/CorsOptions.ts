5dd2c5ba1f627532d94be895e48450cad9b93976
m3yevn <mmm.myintmyat@gmail.com>
2020-05-10 15:30:57 +0800

    😈 add : console log for cors debug temporarily

--- src/config/CorsOptions.ts
+++ src/config/CorsOptions.ts
@@ -7,10 +7,13 @@ var corsOptions = {} as CorsOptions;
 
 if (WHITELIST_IPS && ENVIRONMENT === "PROD") {
   const whitelist = <Array<string>>JSON.parse(WHITELIST_IPS);
+  console.log("Whitelist IP: " + whitelist);
 
   corsOptions = {
     origin: (origin, callback) => {
+      console.log("Origin of request: " + origin);
       if (origin && whitelist.indexOf(origin) !== -1) {
+        console.log("Approved CORS");
         callback(null, true);
       } else {
         callback(
