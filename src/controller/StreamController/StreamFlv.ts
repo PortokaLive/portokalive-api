e37c1fb6479c9d8a08a2ff80013f42be598bf5e5
m3yevn <mmm.myintmyat@gmail.com>
2020-05-24 15:36:49 +0800

    🍊 change: 20secs to timeout stream

--- src/controller/StreamController/StreamFlv.ts
+++ src/controller/StreamController/StreamFlv.ts
@@ -37,7 +37,7 @@ export const getStreamFlv = (req: Request, res: Response) => {
       });
       setTimeout(() => {
         response.destroy();
-      }, 25000);
+      }, 20000);
     }
   });
 };
